cbcb762b879969455744c5fe64073332ec7722a8
chavinlo <85657083+chavinlo@users.noreply.github.com>
2023-07-26 06:33:36 +0000

    basic sdxl

--- data/encoder/reconstruct.py
+++ data/encoder/reconstruct.py
@@ -1,34 +1,43 @@
 
-import diffusers
-import imageio
+from diffusers import AutoencoderKL 
 import numpy as np
 from PIL import Image
 import torch
-import einops
-vae = diffusers.StableDiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5"
-    ).to("cuda").vae
+import cv2
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", torch_dtype=torch.float16).to("cuda")
 
-tmp_tensor = torch.load("/workspace/TempoFunk/video.pt").to("cuda")
+def convert_images_to_video(image_list, output_name, fps):
+    img_size = image_list[0].size
+    size = (img_size[0], img_size[1])
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # or use 'XVID'
+    out = cv2.VideoWriter(output_name, fourcc, fps, size)
+    for i in range(len(image_list)):
+        img_np = cv2.cvtColor(np.array(image_list[i]), cv2.COLOR_RGB2BGR)
+        out.write(img_np)
+    out.release()
+
+tmp_tensor = np.load("/home/ubuntu/10357517.npy")
+tmp_tensor = torch.from_numpy(tmp_tensor).to("cuda")
 print(tmp_tensor.shape)
 
 tmp_tensor = tmp_tensor.unsqueeze(0)
-tmp_tensor = einops.rearrange(tmp_tensor, 'b c f h w -> b f c h w')
+print(tmp_tensor.shape)
+#tmp_tensor = einops.rearrange(tmp_tensor, 'b c f h w -> b f c h w')
+print(tmp_tensor.shape)
 
 frame_list = []
-frame_count = 55
+frame_count = 570
 
 for frame_index in range(frame_count):
-    a_frame = tmp_tensor[:, frame_index, :, :, :]
-    latents = a_frame
+    latents = tmp_tensor[:, frame_index, :, :, :]
+    latents = (1 / vae.config.scaling_factor) * latents
     with torch.no_grad():
-        image = vae.decode(latents).sample
-
+        image = vae.decode(latents, return_dict=False)[0]
     image = (image / 2 + 0.5).clamp(0, 1)
     image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
-    images = (image * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-    frame_list.append(pil_images[0])
+    image = (image * 255).round().astype("uint8")
+    #print(image)
+    frame_list.append(Image.fromarray(image[0]))
     print(frame_index, "/", frame_count)
 
 index = 0
@@ -36,4 +45,4 @@ index = 0
 #     index += 1
 #     img.save(f'/workspace/diffused-video-trainer/outimg/{index}_out.png')
 
-imageio.mimsave('animation.gif', [np.array(img) for img in frame_list], 'GIF', fps=24)
+convert_images_to_video(frame_list, "animation.mp4", 24)
